9eb27158eac8092af6abf660e062ca2226603331
Thorsten Essig <tessig@users.noreply.github.com>
2024-09-23 17:08:50 +0200

    feat: register url prefix sampler for flamingo, resolves #17 (#24)
    Co-authored-by: Carsten Dietrich <3203968+carstendietrich@users.noreply.github.com>

--- sampler.go
+++ sampler.go
@@ -1,73 +1,128 @@
 package opentelemetry
 
 import (
-	"net/http"
+	"fmt"
 	"strings"
 
-	"flamingo.me/flamingo/v3/framework/config"
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
+
+	"flamingo.me/flamingo/v3/framework/config"
 )
 
-type ConfiguredURLPrefixSampler struct {
-	Allowlist            config.Slice
-	Blocklist            config.Slice
-	IgnoreParentDecision bool
+type configuredURLPrefixSampler struct {
+	allowlist []string
+	blocklist []string
 }
 
+// alwaysSampleSpanKindClient enforces sampling of outgoing http requests (client)
+type alwaysSampleSpanKindClient struct {
+	base tracesdk.Sampler
+}
+
+var _ tracesdk.Sampler = (*configuredURLPrefixSampler)(nil)
+var _ tracesdk.Sampler = (*alwaysSampleSpanKindClient)(nil)
+
 // Inject dependencies
-func (c *ConfiguredURLPrefixSampler) Inject(
+func (c *configuredURLPrefixSampler) Inject(
 	cfg *struct {
-		Allowlist            config.Slice `inject:"config:flamingo.opentelemetry.tracing.sampler.allowlist,optional"`
-		Blocklist            config.Slice `inject:"config:flamingo.opentelemetry.tracing.sampler.blocklist,optional"`
-		IgnoreParentDecision bool         `inject:"config:flamingo.opentelemetry.tracing.sampler.ignoreParentDecision,optional"`
+		Allowlist config.Slice `inject:"config:flamingo.opentelemetry.tracing.sampler.allowlist,optional"`
+		Blocklist config.Slice `inject:"config:flamingo.opentelemetry.tracing.sampler.blocklist,optional"`
 	},
-) *ConfiguredURLPrefixSampler {
+) *configuredURLPrefixSampler {
 	if cfg != nil {
-		c.Allowlist = cfg.Allowlist
-		c.Blocklist = cfg.Blocklist
-		c.IgnoreParentDecision = cfg.IgnoreParentDecision
+		var allowed, blocked []string
+
+		err := cfg.Allowlist.MapInto(&allowed)
+		if err != nil {
+			panic(fmt.Errorf("failed to map flamingo.opentelemetry.tracing.sampler.allowlist: %w", err))
+		}
+
+		err = cfg.Blocklist.MapInto(&blocked)
+		if err != nil {
+			panic(fmt.Errorf("failed to map flamingo.opentelemetry.tracing.sampler.blocklist: %w", err))
+		}
+
+		c.allowlist = allowed
+		c.blocklist = blocked
 	}
 
 	return c
 }
 
-func (c *ConfiguredURLPrefixSampler) GetFilterOption() otelhttp.Filter {
-	var allowed, blocked []string
-	_ = c.Allowlist.MapInto(&allowed)
-	_ = c.Blocklist.MapInto(&blocked)
+func (c *configuredURLPrefixSampler) ShouldSample(params tracesdk.SamplingParameters) tracesdk.SamplingResult {
+	psc := trace.SpanContextFromContext(params.ParentContext)
+	path := ""
 
-	return URLPrefixSampler(allowed, blocked, c.IgnoreParentDecision)
+	for _, attr := range params.Attributes {
+		if attr.Key == "http.target" {
+			path = attr.Value.AsString()
+		}
+	}
+
+	// if this is not an incoming request, we decide by parent span
+	if path == "" {
+		decision := tracesdk.Drop
+
+		if psc.IsSampled() {
+			decision = tracesdk.RecordAndSample
+		}
+
+		return tracesdk.SamplingResult{
+			Decision:   decision,
+			Tracestate: psc.TraceState(),
+		}
+	}
+
+	// empty allowed means all
+	sample := len(c.allowlist) == 0
+	// decide if we should sample based on allow list
+	for _, p := range c.allowlist {
+		if strings.HasPrefix(path, p) {
+			sample = true
+			break
+		}
+	}
+
+	// we do not sample, unless the parent is sampled
+	if !sample {
+		return tracesdk.SamplingResult{
+			Decision:   tracesdk.Drop,
+			Tracestate: psc.TraceState(),
+		}
+	}
+
+	// check sampling decision against blocked
+	for _, p := range c.blocklist {
+		if strings.HasPrefix(path, p) {
+			return tracesdk.SamplingResult{
+				Decision:   tracesdk.Drop,
+				Tracestate: psc.TraceState(),
+			}
+		}
+	}
+
+	return tracesdk.SamplingResult{
+		Decision:   tracesdk.RecordAndSample,
+		Tracestate: psc.TraceState(),
+	}
 }
 
-func URLPrefixSampler(allowed, blocked []string, ignoreParentDecision bool) otelhttp.Filter {
-	return func(request *http.Request) bool {
-		path := request.URL.Path
-		isParentSampled := trace.SpanContextFromContext(request.Context()).IsSampled()
-		// empty allowed means all
-		sample := len(allowed) == 0
-		// check allowed if len is > 0, and decide if we should sample
-		for _, p := range allowed {
-			if strings.HasPrefix(path, p) {
-				sample = true
-				break
-			}
-		}
+func (c *configuredURLPrefixSampler) Description() string {
+	allowlist := strings.Join(c.allowlist, ",")
+	blocklist := strings.Join(c.blocklist, ",")
 
-		// we do not sample, unless the parent is sampled
-		if !sample {
-			return !ignoreParentDecision && isParentSampled
-		}
+	return fmt.Sprintf("ConfiguredURLPrefixSampler{allowlist:%s,blocklist:%s}", allowlist, blocklist)
+}
 
-		// check sampling decision against blocked
-		for _, p := range blocked {
-			if strings.HasPrefix(path, p) {
-				sample = false
-				break
-			}
-		}
-
-		// we sample, or the parent sampled
-		return (!ignoreParentDecision && isParentSampled) || sample
+func (s *alwaysSampleSpanKindClient) ShouldSample(parameters tracesdk.SamplingParameters) tracesdk.SamplingResult {
+	if parameters.Kind == trace.SpanKindClient {
+		return tracesdk.AlwaysSample().ShouldSample(parameters)
 	}
+
+	return s.base.ShouldSample(parameters)
+}
+
+func (s *alwaysSampleSpanKindClient) Description() string {
+	return fmt.Sprintf("SpanKindBasedSampler{base:%s}", s.base.Description())
 }
